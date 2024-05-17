9e8812cb72358dc3b59953029e43007f1d39c5f4
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-05-17 20:58:51 +0700

    Update: Create VerificationEmailScreen with VM & Create ChangePasswordScreen with Wm

---
+++ app/src/main/java/com/neirasphere/ecosphere/ui/screen/auth/changepassword/ChangePasswordViewModel.kt
@@ -0,0 +1,7 @@
+package com.neirasphere.ecosphere.ui.screen.auth.changepassword
+
+import androidx.lifecycle.ViewModel
+
+class ChangePasswordViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/neirasphere/ecosphere/ui/screen/auth/verificationemail/VerificationEmailViewModel.kt
@@ -0,0 +1,6 @@
+package com.neirasphere.ecosphere.ui.screen.auth.verificationemail
+
+import androidx.lifecycle.ViewModel
+
+class VerificationEmailViewModel: ViewModel() {
+}

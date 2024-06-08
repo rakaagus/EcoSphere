0095b13c127f204a50efda7ae080de321e11d56c
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-06-08 19:50:22 +0700

    Update: Update Configuration for map, update fine location for home and profile

--- app/src/main/java/com/neirasphere/ecosphere/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/neirasphere/ecosphere/presentation/screen/home/HomeScreen.kt
@@ -1,5 +1,10 @@
 package com.neirasphere.ecosphere.presentation.screen.home
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Geocoder
+import android.location.Location
+import android.os.Looper
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,14 +17,28 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.neirasphere.ecosphere.R
 import com.neirasphere.ecosphere.presentation.components.HomeAppBar
 import com.neirasphere.ecosphere.presentation.components.HomeCardClassify
@@ -27,6 +46,8 @@ import com.neirasphere.ecosphere.presentation.components.SearchBar
 import com.neirasphere.ecosphere.presentation.components.SectionTextColumn
 import com.neirasphere.ecosphere.presentation.common.UiState
 import com.neirasphere.ecosphere.presentation.components.HomeCategoriesLearnCard
+import com.neirasphere.ecosphere.presentation.navigation.Screen
+import java.util.Locale
 
 @Composable
 fun HomeScreen(
@@ -34,13 +55,32 @@ fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
-    HomeContent(viewModel = viewModel, navController = navController, modifier = modifier)
+    var cityName by remember {
+        mutableStateOf("Fetching location...")
+    }
+
+    val context = LocalContext.current
+
+    RequestLocationPermission(onPermissionGranted = {
+        GetUserLocation {
+            cityName = getCityName(context, it)
+        }
+    })
+
+    HomeContent(
+        viewModel = viewModel,
+        moveToProfile = {
+            navController.navigate(Screen.ProfileScreen.route)
+        },
+        locationUser = cityName
+    )
 }
 
 @Composable
 fun HomeContent(
     viewModel: HomeViewModel,
-    navController: NavController,
+    locationUser: String,
+    moveToProfile: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -48,7 +88,7 @@ fun HomeContent(
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
     ) {
-        HomeAppBar(name = "Erlin", location = "Cipayung, Bekasi", navController = navController)
+        HomeAppBar(name = "Erlin", location = locationUser, moveToProfile = moveToProfile)
         SearchBar(query = "", onQueryChange = {}, modifier = Modifier.padding(horizontal = 16.dp))
         HomeCardClassify("10", "20", "30")
         SectionTextColumn(title = R.string.section_one, modifier = Modifier.padding(top = 25.dp)) {
@@ -85,6 +125,82 @@ fun HomeContent(
                     .padding(horizontal = 16.dp)
             )
         }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun RequestLocationPermission(
+    onPermissionGranted: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        permissionState.launchMultiplePermissionRequest()
+    }
+
+    when {
+        permissionState.allPermissionsGranted -> {
+            onPermissionGranted()
+        }
+
+        else -> {
+
+        }
+    }
+}
+
+@SuppressLint("MissingPermission")
+@Composable
+fun GetUserLocation(
+    onLocationReceived: (Location) -> Unit,
+) {
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(LocalContext.current)
+    val locationCallback = rememberUpdatedState(newValue = onLocationReceived)
+
+    DisposableEffect(key1 = Unit) {
+        val locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val locationListener = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let { location ->
+                    locationCallback.value(location)
+                }
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationListener,
+            Looper.getMainLooper()
+        )
+        onDispose {
+            fusedLocationClient.removeLocationUpdates(locationListener)
+        }
+    }
+}
+
+fun getCityName(
+    context: Context,
+    location: Location
+) : String {
+    val geocoder = Geocoder(context, Locale.getDefault())
+    val address = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+    return if (!address.isNullOrEmpty()) {
+        address[0].locality ?: "Unknown City"
+    } else {
+        "Unknown City"
     }
 }
 
--- app/src/main/java/com/neirasphere/ecosphere/presentation/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/neirasphere/ecosphere/presentation/screen/profile/ProfileScreen.kt
@@ -1,5 +1,11 @@
 package com.neirasphere.ecosphere.presentation.screen.profile
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Geocoder
+import android.location.Location
+import android.os.Looper
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,10 +17,24 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.neirasphere.ecosphere.R
 import com.neirasphere.ecosphere.presentation.components.ButtonProfile
 import com.neirasphere.ecosphere.presentation.components.HeaderProfile
@@ -22,12 +42,25 @@ import com.neirasphere.ecosphere.presentation.components.SectionProfile
 import com.neirasphere.ecosphere.presentation.components.SectionTextColumnProfile
 import com.neirasphere.ecosphere.presentation.navigation.Screen
 import com.neirasphere.ecosphere.ui.theme.NeutralColorGrey
+import java.util.Locale
 
 @Composable
 fun ProfileScreen(
     navController: NavController,
     modifier: Modifier = Modifier
 ) {
+    var cityName by remember {
+        mutableStateOf("Fetching location...")
+    }
+
+    val context = LocalContext.current
+
+    RequestLocationPermission(onPermissionGranted = {
+        GetUserLocation {
+            cityName = getCityName(context, it)
+        }
+    })
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -45,7 +78,8 @@ fun ProfileScreen(
             moveToHelpScreen = {},
             moveToReport = {},
             onLogoutClick = {},
-            clickFreeUp = {}
+            clickFreeUp = {},
+            locationUser = cityName
         )
     }
 }
@@ -60,12 +94,13 @@ fun ProfileContent(
     clickFreeUp: () -> Unit,
     moveToReport: () -> Unit,
     onLogoutClick: () -> Unit,
+    locationUser: String,
     modifier: Modifier = Modifier
 ) {
     HeaderProfile(
         image = R.drawable.example_image_user,
         name = "Erlin",
-        location = "Piyungan, Yogyakarta"
+        location = locationUser
     )
     SectionTextColumnProfile(title = R.string.header_title_1) {
         Card(
@@ -142,4 +177,80 @@ fun ProfileContent(
         click = {
             onLogoutClick()
         })
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun RequestLocationPermission(
+    onPermissionGranted: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        permissionState.launchMultiplePermissionRequest()
+    }
+
+    when {
+        permissionState.allPermissionsGranted -> {
+            onPermissionGranted()
+        }
+
+        else -> {
+
+        }
+    }
+}
+
+@SuppressLint("MissingPermission")
+@Composable
+fun GetUserLocation(
+    onLocationReceived: (Location) -> Unit,
+) {
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(LocalContext.current)
+    val locationCallback = rememberUpdatedState(newValue = onLocationReceived)
+
+    DisposableEffect(key1 = Unit) {
+        val locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val locationListener = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let { location ->
+                    locationCallback.value(location)
+                }
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationListener,
+            Looper.getMainLooper()
+        )
+        onDispose {
+            fusedLocationClient.removeLocationUpdates(locationListener)
+        }
+    }
+}
+
+fun getCityName(
+    context: Context,
+    location: Location
+) : String {
+    val geocoder = Geocoder(context, Locale.getDefault())
+    val address = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+    return if (!address.isNullOrEmpty()) {
+        address[0].locality ?: "Unknown City"
+    } else {
+        "Unknown City"
+    }
 }

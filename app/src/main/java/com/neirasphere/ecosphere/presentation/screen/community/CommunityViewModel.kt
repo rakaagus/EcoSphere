9366a247956945cae608a0486431c877d91c35b7
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-16 11:50:40 +0700

    Update: Update post function (Copying to Classify because Classify use Multipart too)

--- app/src/main/java/com/neirasphere/ecosphere/presentation/screen/community/CommunityViewModel.kt
+++ app/src/main/java/com/neirasphere/ecosphere/presentation/screen/community/CommunityViewModel.kt
@@ -1,13 +1,16 @@
 package com.neirasphere.ecosphere.presentation.screen.community
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.neirasphere.ecosphere.ResultDefault
+import com.neirasphere.ecosphere.data.remote.response.CommunityPostResponse
 import com.neirasphere.ecosphere.domain.repository.CommunityRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import retrofit2.Call
+import retrofit2.Response
+import java.io.File
 import javax.inject.Inject
 
 class CommunityViewModel @Inject constructor(
@@ -17,34 +20,35 @@ class CommunityViewModel @Inject constructor(
     private val _postState = MutableStateFlow(PostState())
     val postState = _postState.asStateFlow()
 
-    fun post(content: String) = viewModelScope.launch {
-        repository.post(content).collect { result ->
-            when (result) {
-                is ResultDefault.Loading -> {
-                    _postState.update {
-                        it.copy(
-                            isLoading = true
-                        )
-                    }
-                }
-                is ResultDefault.Success -> {
-                    _postState.update {
-                        it.copy(
-                            isSuccess = true,
-                            isLoading = false
-                        )
-                    }
-                }
-                is ResultDefault.Error -> {
-                    _postState.update {
-                        it.copy(
-                            isError = result.error,
-                            isConnectLoading = true
-                        )
-                    }
+    fun post(post: String, img: File) {
+        _postState.value = PostState(isLoading = true)
+
+        val requestFile = img.asRequestBody("image/*".toMediaTypeOrNull())
+        val body = MultipartBody.Part.createFormData("post_img", img.name, requestFile)
+
+        repository.post(
+            post,
+            "00-00-0000 00:00:00",
+            body
+        ).enqueue(object :
+            retrofit2.Callback<CommunityPostResponse> {
+            override fun onResponse(
+                call: Call<CommunityPostResponse>,
+                response: Response<CommunityPostResponse>
+            ) {
+                if (response.isSuccessful) {
+                    _postState.value = PostState(isSuccess = true)
+                } else {
+                    _postState.value = PostState(isError = "Post failed: ${response.errorBody()?.string()}")
                 }
             }
-        }
+
+            override fun onFailure(call: Call<CommunityPostResponse>, t: Throwable) {
+                _postState.value = PostState(isError = t.message)
+            }
+            }
+        )
+
     }
 
 }

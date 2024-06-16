d5ddc05fbb75c2e5afed9b6881c3069af9c483fc
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-16 11:46:15 +0700

    Update: Add post and get Function in ApiService

--- app/src/main/java/com/neirasphere/ecosphere/data/remote/ApiService.kt
+++ app/src/main/java/com/neirasphere/ecosphere/data/remote/ApiService.kt
@@ -1,10 +1,17 @@
 package com.neirasphere.ecosphere.data.remote
 
-import com.neirasphere.ecosphere.data.remote.response.CommunityResponse
+import com.neirasphere.ecosphere.data.remote.response.CommunityPostResponse
+import com.neirasphere.ecosphere.data.remote.response.GetAllPostResponse
 import com.neirasphere.ecosphere.data.remote.response.LoginResponse
+import okhttp3.MultipartBody
+import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ApiService {
 
@@ -15,10 +22,17 @@ interface ApiService {
         @Field("password") password: String
     ): LoginResponse
 
-    @FormUrlEncoded
+    @Multipart
     @POST("community/")
-    suspend fun post(
-        @Field("content") content: String
-    ): CommunityResponse
+    fun post(
+        @Part("post") post: String,
+        @Part("create_at") createdAt: String,
+        @Part("post_img") postImg: MultipartBody.Part
+    ): Call<CommunityPostResponse>
+
+    @GET("/community/posts")
+    suspend fun getAllCommunityPosts(
+        @Header("Authorization") token: String
+    ): GetAllPostResponse
 
 }

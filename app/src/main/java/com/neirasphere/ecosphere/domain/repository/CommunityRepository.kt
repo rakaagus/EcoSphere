db2842091f75b26c99bea396d5952f89cb5ede47
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-20 00:19:40 +0700

    Update: Changing getAllCommunityPost return type and splitting post to post and postWithImage

--- app/src/main/java/com/neirasphere/ecosphere/domain/repository/CommunityRepository.kt
+++ app/src/main/java/com/neirasphere/ecosphere/domain/repository/CommunityRepository.kt
@@ -1,15 +1,19 @@
 package com.neirasphere.ecosphere.domain.repository
 
 import com.neirasphere.ecosphere.data.CommunityResult
-import com.neirasphere.ecosphere.data.remote.response.DataItem
+import com.neirasphere.ecosphere.data.remote.response.CommunityPostResponse
 import com.neirasphere.ecosphere.domain.model.CommunityPost
+import com.neirasphere.ecosphere.domain.model.CommunityPostSQL
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
 
 interface CommunityRepository {
 
     val communityPost : MutableList<CommunityPost>
 
-    fun getAllCommunityPost(token: String) : Flow<CommunityResult<List<DataItem?>?>>
+    fun getAllCommunityPost() : Flow<CommunityResult<MutableList<CommunityPostSQL>>>
 
-//    fun post(post: String, createdAt: String, postImg: MultipartBody.Part): Flow<Result<CommunityPostResponse>>
+    fun postWithImage(post: String, postImg: MultipartBody.Part): Flow<CommunityResult<CommunityPostResponse>>
+
+    fun post(post: String): Flow<CommunityResult<CommunityPostResponse>>
 }

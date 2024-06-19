ccf29f11d3578d9f607be287689935f845e3a84e
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-20 00:22:23 +0700

    Update: Changing postImg type to String, Adding createdAt and imgProfile

--- app/src/main/java/com/neirasphere/ecosphere/data/remote/response/CommunityResponse.kt
+++ app/src/main/java/com/neirasphere/ecosphere/data/remote/response/CommunityResponse.kt
@@ -16,14 +16,11 @@ data class GetAllPostResponse(
     val message: String? = null
 )
 
-data class PostImg(
-    val data: List<Int?>? = null,
-    val type: String? = null
-)
-
 data class DataItem(
     @field:SerializedName("post_img")
-    val postImg: PostImg? = null,
+    val postImg: String? = null,
+    @field:SerializedName("created_at")
+    val createdAt: String? = null,
     @field:SerializedName("post")
     val post: String? = null,
     @field:SerializedName("id_user")
@@ -31,5 +28,8 @@ data class DataItem(
     @field:SerializedName("communityId")
     val communityId: Int? = null,
     @field:SerializedName("email")
-    val email: String? = null
+    val email: String? = null,
+    @field:SerializedName("img_profile")
+    val imgProfile: String? = null
+
 )

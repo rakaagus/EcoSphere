c469bc9b4d5573123f4a8d634a6d1c89c6edb232
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-20 00:25:04 +0700

    Update: Adding hilt, delete token from getAllCommunityPost params, splitting post to post and postWithImage

--- app/src/main/java/com/neirasphere/ecosphere/presentation/screen/community/CommunityViewModel.kt
+++ app/src/main/java/com/neirasphere/ecosphere/presentation/screen/community/CommunityViewModel.kt
@@ -5,12 +5,18 @@ import androidx.lifecycle.viewModelScope
 import com.neirasphere.ecosphere.data.CommunityResult
 import com.neirasphere.ecosphere.domain.repository.AppRepository
 import com.neirasphere.ecosphere.domain.repository.CommunityRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 import javax.inject.Inject
 
+@HiltViewModel
 class CommunityViewModel @Inject constructor(
     private val repository: CommunityRepository,
     private val appRepository: AppRepository
@@ -23,11 +29,11 @@ class CommunityViewModel @Inject constructor(
     val getPostsState = _getPostsState.asStateFlow()
 
     init {
-        getAllCommunityPosts(appRepository.getToken().toString())
+        getAllCommunityPosts()
     }
 
-    private fun getAllCommunityPosts(token: String) = viewModelScope.launch {
-        repository.getAllCommunityPost(token).collect { result ->
+    private fun getAllCommunityPosts() = viewModelScope.launch {
+        repository.getAllCommunityPost().collect { result ->
             when (result) {
                 is CommunityResult.Error -> _getPostsState.update {
                     it.copy(
@@ -52,35 +58,73 @@ class CommunityViewModel @Inject constructor(
         }
     }
 
-//    fun post(post: String, img: File) {
-//        _postState.value = PostState(isLoading = true)
-//
-//        val requestFile = img.asRequestBody("image/*".toMediaTypeOrNull())
-//        val body = MultipartBody.Part.createFormData("post_img", img.name, requestFile)
-//
-//        repository.post(
-//            post,
-//            "00-00-0000 00:00:00",
-//            body
-//        ).enqueue(object :
-//            retrofit2.Callback<CommunityPostResponse> {
-//            override fun onResponse(
-//                call: Call<CommunityPostResponse>,
-//                response: Response<CommunityPostResponse>
-//            ) {
-//                if (response.isSuccessful) {
-//                    _postState.value = PostState(isSuccess = true)
-//                } else {
-//                    _postState.value = PostState(isError = "Post failed: ${response.errorBody()?.string()}")
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<CommunityPostResponse>, t: Throwable) {
-//                _postState.value = PostState(isError = t.message)
-//            }
-//            }
-//        )
-//
-//    }
+    fun postWithImage(post: String, img: File) = viewModelScope.launch {
+        val requestFile = img.asRequestBody("image/*".toMediaTypeOrNull())
+        val body = MultipartBody.Part.createFormData("post_img", img.name, requestFile)
+
+        repository.postWithImage(
+            post,
+            body
+        ).collect{ result ->
+            when (result) {
+                is CommunityResult.Loading -> {
+                    _postState.update {
+                        it.copy(
+                            isLoading = true
+                        )
+                    }
+                }
+                is CommunityResult.Success -> {
+                    _postState.update {
+                        it.copy(
+                            isLoading = false,
+                            isSuccess = true
+                        )
+                    }
+                }
+                is CommunityResult.Error -> {
+                    _postState.update {
+                        it.copy(
+                            isLoading = false,
+                            isError = result.message
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun post(post: String) = viewModelScope.launch {
+
+        repository.post(
+            post
+        ).collect{ result ->
+            when (result) {
+                is CommunityResult.Loading -> {
+                    _postState.update {
+                        it.copy(
+                            isLoading = true
+                        )
+                    }
+                }
+                is CommunityResult.Success -> {
+                    _postState.update {
+                        it.copy(
+                            isLoading = false,
+                            isSuccess = true
+                        )
+                    }
+                }
+                is CommunityResult.Error -> {
+                    _postState.update {
+                        it.copy(
+                            isLoading = false,
+                            isError = result.message
+                        )
+                    }
+                }
+            }
+        }
+    }
 
 }

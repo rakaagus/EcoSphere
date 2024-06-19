3eadf930c775962fc8e64f7c103d4e9bffc40f75
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-20 00:17:10 +0700

    Update: splitting post to post and postWithImage

--- app/src/main/java/com/neirasphere/ecosphere/data/remote/ApiService.kt
+++ app/src/main/java/com/neirasphere/ecosphere/data/remote/ApiService.kt
@@ -1,12 +1,16 @@
 package com.neirasphere.ecosphere.data.remote
 
+import com.neirasphere.ecosphere.data.remote.response.CommunityPostResponse
 import com.neirasphere.ecosphere.data.remote.response.GetAllPostResponse
 import com.neirasphere.ecosphere.data.remote.response.LoginResponse
+import okhttp3.MultipartBody
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ApiService {
 
@@ -17,15 +21,22 @@ interface ApiService {
         @Field("password") password: String
     ): LoginResponse
 
-//    @Multipart
-//    @POST("community/")
-//    suspend fun post(
-//        @Part("post") post: String,
-//        @Part("created_at") createdAt: String,
-//        @Part("post_img") postImg: MultipartBody.Part
-//    ): CommunityPostResponse
+    @Multipart
+    @POST("community/")
+    suspend fun postWithImage(
+        @Header("Authorization") token: String,
+        @Part("post") post: String,
+        @Part("post_img") postImg: MultipartBody.Part
+    ): CommunityPostResponse
 
-    @GET("/community/posts")
+    @FormUrlEncoded
+    @POST("community/")
+    suspend fun post(
+        @Header("Authorization") token: String,
+        @Field("post") post: String
+    ): CommunityPostResponse
+
+    @GET("community/posts")
     suspend fun getAllCommunityPosts(
         @Header("Authorization") token: String
     ): GetAllPostResponse

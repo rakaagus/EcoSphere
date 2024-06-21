f97e31d51d31242ba9baf310d96ea35af0ef8d08
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-21 20:31:39 +0700

    Update: Rearrange items in GetAllPostResponse and DataItem, Add GetPostLikesResponse, LikeItem, GetACommentResponse, CommentItem

--- app/src/main/java/com/neirasphere/ecosphere/data/remote/response/CommunityResponse.kt
+++ app/src/main/java/com/neirasphere/ecosphere/data/remote/response/CommunityResponse.kt
@@ -10,26 +10,70 @@ data class CommunityPostResponse(
 )
 
 data class GetAllPostResponse(
+
     @field:SerializedName("data")
     val data: List<DataItem?>? = null,
+    @field:SerializedName("success")
+    val success: Boolean? = null,
     @field:SerializedName("message")
     val message: String? = null
 )
 
 data class DataItem(
-    @field:SerializedName("post_img")
-    val postImg: String? = null,
-    @field:SerializedName("created_at")
-    val createdAt: String? = null,
-    @field:SerializedName("post")
-    val post: String? = null,
-    @field:SerializedName("id_user")
-    val idUser: Int? = null,
+
     @field:SerializedName("communityId")
     val communityId: Int? = null,
+    @field:SerializedName("post")
+    val post: String? = null,
+    @field:SerializedName("post_img")
+    val postImg: String? = null,
+    @field:SerializedName("id_user")
+    val idUser: Int? = null,
     @field:SerializedName("email")
     val email: String? = null,
     @field:SerializedName("img_profile")
-    val imgProfile: String? = null
+    val imgProfile: String? = null,
+    @field:SerializedName("created_at")
+    val createdAt: String? = null
 
+)
+
+data class GetPostLikesResponse(
+
+    @field:SerializedName("message")
+    val message: String,
+    @field:SerializedName("success")
+    val success: Boolean? = null,
+    @field:SerializedName("data")
+    val data: List<LikeItem?>? = null
+
+)
+
+data class LikeItem(
+
+    @field:SerializedName("likeId")
+    val likeId: Int? = null,
+    @field:SerializedName("communityId")
+    val communityId: Int? = null,
+    @field:SerializedName("id_user")
+    val idUser: Int? = null,
+    @field:SerializedName("email")
+    val email: String? = null
+
+)
+
+data class GetACommentResponse(
+    val data: List<CommentItem?>? = null,
+    val success: Boolean? = null,
+    val message: String? = null
+)
+
+data class CommentItem(
+    val commentImg: Any? = null,
+    val commentId: Int? = null,
+    val createdAt: String? = null,
+    val comment: String? = null,
+    val idUser: Int? = null,
+    val communityId: Int? = null,
+    val email: String? = null
 )
